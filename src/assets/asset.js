5c7a70d1c4d6d50db9fec670140eaf000dbb281f
no name <your-email@example.com>
2025-07-03 00:27:54 +0530

    added assets of the skills and skills carousel jsx file to the project

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -1,7 +1,19 @@
 import portfolioLogo from "./logo/portfolio-logo.png";
 import profilePic from "./logo/profile-pic.png";
-import expImgWhite from './experience/expImgWhite.svg';
-import expImgBlack from './experience/expblack.svg';
+import expImgWhite from "./experience/expImgWhite.svg";
+import expImgBlack from "./experience/expblack.svg";
+
+// technology - programming logo
+import css from "./technology/css.svg";
+import html from "./technology/html.svg";
+import java from "./technology/java.svg";
+import javascript from "./technology/javascript.svg";
+import mongodb from "./technology/mongoDB.svg";
+import mysql from "./technology/mysql.svg";
+import nodejs from "./technology/nodejs.svg";
+import react from "./technology/react.svg";
+import springboot from "./technology/spring-boot.svg";
+import spring from "./technology/spring.svg";
 
 export const assets = {
   portfolioLogo,
@@ -27,5 +39,58 @@ export const experienceData = [
     jobtitle: "Java Developer",
     startYear: "2022",
     endYear: "2023",
+  },
+];
+
+export const skillsData = [
+  {
+    id: 1,
+    name: "Java",
+    image: java,
+  },
+  {
+    id: 2,
+    name: "Spring",
+    image: spring,
+  },
+  {
+    id: 3,
+    name: "Spring Boot",
+    image: springboot,
+  },
+  {
+    id: 5,
+    name: "JavaScript",
+    image: javascript,
+  },
+  {
+    id: 6,
+    name: "React",
+    image: react,
+  },
+  {
+    id: 7,
+    name: "Node JS",
+    image: nodejs,
+  },
+  {
+    id: 8,
+    name: "HTML",
+    image: html,
+  },
+  {
+    id: 9,
+    name: "CSS",
+    image: css,
+  },
+  {
+    id: 10,
+    name: "MySQL",
+    image: mysql,
+  },
+  {
+    id: 11,
+    name: "Mongo DB",
+    image: mongodb,
   },
 ];
