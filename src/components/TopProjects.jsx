8d63afaf7b639547b0e1a7c171d970027474a88e
no name <your-email@example.com>
2025-07-04 18:51:10 +0530

    changing the css of project title

--- src/components/TopProjects.jsx
+++ src/components/TopProjects.jsx
@@ -5,7 +5,9 @@ import Card from "./ui/Card";
 const TopProjects = () => {
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
-      <h1 className="text-3xl font-medium text-gray-200">Top Projects</h1>
+      <h1 className="pb-10 pr-5 text-center text-5xl md:text-6xl md:text-end lg:text-7xl font-semibold bg-gradient-to-r from-purple-500 to-gray-300 bg-clip-text text-transparent leading-tight md:leading-tight lg:leading-tight">
+          Projects
+        </h1>
       <p className="sm:w-1/3 text-center text-sm"></p>
       <div className="w-full grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 sm:px-0">
         {topProjects.map((item, index) => (
